a55a9e8541f174632e9bb1bc9dcef55e9ac7286b
DanMB <danielmillst@gmail.com>
2024-12-10 05:49:39 -0500

    Routing

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,21 +22,40 @@ class CounterApp extends StatelessWidget {
   Widget build(BuildContext context) => MUT(
         rebuildChildren: true,
         value: 0,
-        builder: (context) => Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text('You have pushed the button this many times:'),
-                Text('${context.pylon<int>()}'),
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () => context.modPylon<int>((t) => t + 1),
-            tooltip: 'Increment',
-            child: const Icon(Icons.add),
-          ),
-        ),
+        builder: (context) => Pylon<int>(
+            value: context.pylon<int>(),
+            broadcast: "counter",
+            builder: (context) => Scaffold(
+                  body: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        const Text(
+                            'You have pushed the button this many times:'),
+                        Text('${context.pylon<int>()}'),
+                        Text(
+                            '${Pylon.visibleBroadcastToUri(context, Uri.base)}'),
+                        ElevatedButton(
+                            onPressed: () => Pylon.push(context, OtherScreen()),
+                            child: Text("Test route"))
+                      ],
+                    ),
+                  ),
+                  floatingActionButton: FloatingActionButton(
+                    onPressed: () => context.modPylon<int>((t) => t + 1),
+                    tooltip: 'Increment',
+                    child: const Icon(Icons.add),
+                  ),
+                )),
       );
+}
+
+class OtherScreen extends StatelessWidget {
+  const OtherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => PylonPort<int>(
+      pull: "counter",
+      builder: (context) =>
+          Scaffold(body: Text("Value pulled as ${context.pylon<int>()}")));
 }
--- lib/pylon.dart
+++ lib/pylon.dart
@@ -1,11 +1,25 @@
 library pylon;
 
+import 'dart:html' as html;
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:rxdart/rxdart.dart';
+import 'package:toxic/extensions/future.dart';
 import 'package:toxic/extensions/iterable.dart';
 
 typedef PylonBuilder = Widget Function(BuildContext context);
+const List<PylonCodec> pylonStandardCodecs = [
+  LiteralPylonCodec<String>(),
+  LiteralPylonCodec<int>(),
+  LiteralPylonCodec<double>(),
+  LiteralPylonCodec<bool>(),
+];
+
+List<PylonCodec> _flatCodecs = [];
 
 /// Represents common route types for [Pylon.push]
 enum PylonRouteType {
@@ -76,6 +90,7 @@ class Pylon<T> extends StatelessWidget {
   final T value;
   final PylonBuilder? builder;
   final Widget? child;
+  final String? broadcast;
 
   /// If local is set to true it wont persist across navigation routes
   final bool local;
@@ -84,6 +99,7 @@ class Pylon<T> extends StatelessWidget {
       {super.key,
       required this.value,
       required this.builder,
+      this.broadcast,
       this.local = false})
       : child = null;
 
@@ -92,12 +108,20 @@ class Pylon<T> extends StatelessWidget {
   /// then it wont be available until either a builder function is used or the child widget build method uses it
   /// Use the regular constructor for lazy inlining
   const Pylon.withChild(
-      {super.key, required this.value, required this.child, this.local = false})
+      {super.key,
+      required this.value,
+      required this.child,
+      this.broadcast,
+      this.local = false})
       : builder = null;
+
+  dynamic encodeRaw(BuildContext context) =>
+      context.pylonCodec<T>().pylonToValue(value);
 
   /// This is primarily used for [PylonCluster]. Using this constructor produces a widget which will
   /// throw an error if built as it doesnt have a child or builder function
-  const Pylon.data({super.key, required this.value, this.local = false})
+  const Pylon.data(
+      {super.key, required this.value, this.local = false, this.broadcast})
       : builder = null,
         child = null;
 
@@ -116,16 +140,20 @@ class Pylon<T> extends StatelessWidget {
     Widget child, {
     PylonRouteType type = PylonRouteType.material,
     Route<T>? route,
-  }) =>
-      Navigator.push(
-          context,
-          route ??
-              switch (type) {
-                PylonRouteType.material =>
-                  Pylon.materialPageRoute(context, (context) => child),
-                PylonRouteType.cupertino =>
-                  Pylon.cupertinoPageRoute(context, (context) => child),
-              });
+  }) {
+    UriPylonCodecUtils.updateUri(context);
+
+    return Navigator.push<T?>(
+            context,
+            route ??
+                switch (type) {
+                  PylonRouteType.material =>
+                    Pylon.materialPageRoute(context, (context) => child),
+                  PylonRouteType.cupertino =>
+                    Pylon.cupertinoPageRoute(context, (context) => child),
+                })
+        .thenRun((_) => UriPylonCodecUtils.updateUri(context));
+  }
 
   /// Creates a [MaterialPageRoute] with the [Pylon] widgets mirrored into the builder function to transfer the values
   static MaterialPageRoute<T> materialPageRoute<T extends Object?>(
@@ -136,6 +164,25 @@ class Pylon<T> extends StatelessWidget {
   static CupertinoPageRoute<T> cupertinoPageRoute<T extends Object?>(
           BuildContext context, Widget Function(BuildContext) builder) =>
       CupertinoPageRoute<T>(builder: mirror(context, builder));
+
+  static Iterable<Pylon> visibleBroadcastingPylons(BuildContext context,
+          {bool ignoreLocals = false}) =>
+      visiblePylons(context, ignoreLocals: ignoreLocals)
+          .where((i) => i.broadcast != null);
+
+  static Uri visibleBroadcastToUri(BuildContext context, Uri uri,
+          {bool ignoreLocals = false}) =>
+      uri.replace(queryParameters: {
+        ...uri.queryParameters,
+        ...visibleBroadcastMap(context, ignoreLocals: ignoreLocals)
+      });
+
+  static Map<String, String> visibleBroadcastMap(BuildContext context,
+          {bool ignoreLocals = false}) =>
+      Map.fromEntries(
+          visibleBroadcastingPylons(context, ignoreLocals: ignoreLocals).map(
+              (p) => MapEntry(p.broadcast!,
+                  UriPylonCodecUtils.toUrlValue(p.encodeRaw(context)))));
 
   static List<Pylon> visiblePylons(BuildContext context,
       {bool ignoreLocals = false}) {
@@ -160,18 +207,21 @@ class Pylon<T> extends StatelessWidget {
     return providers;
   }
 
+  static void registerCodec(PylonCodec codec) {
+    _flatCodecs.add(codec);
+  }
+
   /// Creates a builder function which produces a PylonCluster of all visible ancestor pylons
   /// from [context] and uses the provided [builder] function. Use this when building custom routes
   static Widget Function(BuildContext) mirror(
       BuildContext context, Widget Function(BuildContext) builder) {
     List<Pylon> providers = visiblePylons(context, ignoreLocals: true);
 
-    Widget result = PylonCluster(
-      pylons: providers.reversed.toList(),
-      builder: builder,
-    );
-
-    return (context) => result;
+    return (context) => PylonBroadcaster(
+        builder: (context) => PylonCluster(
+              pylons: providers.reversed.toList(),
+              builder: builder,
+            ));
   }
 
   @override
@@ -236,7 +286,10 @@ extension XPylonIterable<T> on Iterable<T> {
       map((e) => Pylon<T>(value: e, builder: builder)).toList();
 }
 
-extension XContext on BuildContext {
+extension XContextPylon on BuildContext {
+  /// Returns true if a pylon is available for the given type
+  bool hasPylon<T>({Type? runtime}) => pylonOr<T>(runtime: runtime) != null;
+
   /// Returns the value of the nearest ancestor [Pylon] widget of type T or null
   T? pylonOr<T>({Type? runtime}) => runtime != null
       ? Pylon.visiblePylons(this)
@@ -336,4 +389,223 @@ class MutablePylonState<T> extends State<MutablePylon> {
         builder: widget.builder,
         local: widget.local,
       );
+}
+
+// Represents a codec for pylon value types
+abstract class PylonCodec<T> {
+  const PylonCodec();
+
+  dynamic pylonToValue(T t);
+
+  Future<T> pylonFromValue(dynamic d);
+}
+
+class LiteralPylonCodec<T> extends PylonCodec<T> {
+  const LiteralPylonCodec();
+
+  @override
+  Future<T> pylonFromValue(d) async => d as T;
+
+  @override
+  dynamic pylonToValue(T t) => t;
+}
+
+extension XContextPylonCodec on BuildContext {
+  PylonCodec<T>? _standardPylonCodec<T>() =>
+      pylonStandardCodecs.whereType<PylonCodec<T>>().firstOrNull;
+
+  PylonCodec<T>? _flatPylonCodec<T>() =>
+      _flatCodecs.whereType<PylonCodec<T>>().firstOrNull;
+
+  PylonCodec<T> pylonCodec<T>() =>
+      _standardPylonCodec<T>() ??
+      _flatPylonCodec<T>() ??
+      pylon<PylonCodec<T>>();
+
+  PylonCodec<T>? pylonCodecOr<T>() =>
+      _standardPylonCodec<T>() ??
+      _flatPylonCodec<T>() ??
+      pylonOr<PylonCodec<T>>();
+
+  bool hasPylonCodec<T>() => pylonCodecOr<T>() != null;
+
+  String pylonEncode<T>(T value) =>
+      UriPylonCodecUtils.toUrlValue(pylonCodec<T>().pylonToValue(value));
+
+  Future<T> pylonDecode<T>(String value) =>
+      pylonCodec<T>().pylonFromValue(UriPylonCodecUtils.fromUrlValue(value));
+}
+
+class UriPylonCodecUtils {
+  static void setUri(Uri uri) {
+    if (kIsWeb) {
+      html.window.history.pushState(null, "", uri.toString());
+    }
+  }
+
+  static Uri getUri() {
+    if (kIsWeb) {
+      return Uri.parse(html.window.location.href);
+    }
+    return Uri.base;
+  }
+
+  static String toUrlValue(dynamic value) {
+    String out = "xENCODE_ERROR";
+
+    if (value is String) {
+      out = "s$value";
+    } else if (value is int) {
+      out = "i$value";
+    } else if (value is double) {
+      out = "d$value";
+    } else if (value is bool) {
+      out = "b${value ? "y" : "n"}";
+    } else if (value is DateTime) {
+      out = "t${value.toIso8601String()}";
+    } else {
+      out = "j${jsonEncode(value)}";
+    }
+
+    out = "*$out";
+
+    return out;
+  }
+
+  static dynamic fromUrlValue(String value) {
+    assert(value.startsWith("*"));
+    String t = value.substring(1, 2);
+    String v = value.substring(2);
+
+    if (t == "s") {
+      return v;
+    } else if (t == "i") {
+      return int.parse(v);
+    } else if (t == "d") {
+      return double.parse(v);
+    } else if (t == "b") {
+      return v == "1";
+    } else if (t == "t") {
+      return DateTime.parse(v);
+    } else if (t == "j") {
+      return jsonDecode(v);
+    } else {
+      throw "Unknown type: $t";
+    }
+  }
+
+  static String doubleToBase36String(double value, {int decimalPlaces = 6}) {
+    final integerPart = value.floor();
+    final fraction = value - integerPart;
+    final fractionAsInt = (fraction * pow(10, decimalPlaces)).round();
+    final encodedInt = integerPart.toRadixString(36);
+    final encodedFrac = fractionAsInt.toRadixString(36);
+
+    return '$encodedInt.$encodedFrac';
+  }
+
+  static double base36StringToDouble(String base36String,
+      {int decimalPlaces = 6}) {
+    final parts = base36String.split('.');
+    if (parts.length != 2) {
+      throw const FormatException('Expected format "int.frac" in base36');
+    }
+
+    final intPart = int.parse(parts[0], radix: 36);
+    final fracPart = int.parse(parts[1], radix: 36);
+    final fracString = fracPart.toString().padLeft(decimalPlaces, '0');
+    final doubleString = '$intPart.$fracString';
+
+    return double.parse(doubleString);
+  }
+
+  static void updateUri(BuildContext context) {
+    if (kIsWeb) {
+      Future.delayed(
+          const Duration(milliseconds: 1000),
+          () => UriPylonCodecUtils.setUri(Pylon.visibleBroadcastToUri(
+              context, UriPylonCodecUtils.getUri())));
+    }
+  }
+}
+
+class PylonBroadcaster extends StatefulWidget {
+  final PylonBuilder builder;
+
+  const PylonBroadcaster({super.key, required this.builder});
+
+  @override
+  State<PylonBroadcaster> createState() => _PylonBroadcasterState();
+}
+
+class _PylonBroadcasterState extends State<PylonBroadcaster> {
+  @override
+  void initState() {
+    if (kIsWeb) {
+      UriPylonCodecUtils.setUri(
+          Pylon.visibleBroadcastToUri(context, UriPylonCodecUtils.getUri()));
+    }
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) => Builder(builder: widget.builder);
+}
+
+class PylonPort<T> extends StatefulWidget {
+  final String pull;
+  final PylonBuilder builder;
+  final bool local;
+  final String? rebroadcast;
+
+  const PylonPort(
+      {super.key,
+      required this.pull,
+      required this.builder,
+      this.local = true,
+      this.rebroadcast});
+
+  @override
+  State<PylonPort<T>> createState() => _PylonPortState<T>();
+}
+
+class _PylonPortState<T> extends State<PylonPort<T>> {
+  Future<T>? future;
+
+  @override
+  Widget build(BuildContext context) {
+    T? value = Pylon.visiblePylons(context)
+        .whereType<Pylon<T>>()
+        .select((i) => i.broadcast != null)
+        ?.value;
+
+    if (value == null) {
+      if (future == null) {
+        Map<String, String> q = UriPylonCodecUtils.getUri().queryParameters;
+
+        if (q.containsKey(widget.pull)) {
+          future = context.pylonDecode<T>(q[widget.pull]!);
+        } else {
+          throw "PylonPort: No port found for ${widget.pull} broadcasting on Uri ${UriPylonCodecUtils.getUri()}. No context value was available either. Avalable: ${q}";
+        }
+      }
+
+      return FutureBuilder<T>(
+          future: future,
+          builder: (context, snap) => Pylon<T?>(
+                value: snap.data,
+                builder: widget.builder,
+                local: widget.local,
+                broadcast: widget.rebroadcast,
+              ));
+    }
+
+    return Pylon<T>(
+      value: value,
+      local: widget.local,
+      broadcast: widget.rebroadcast,
+      builder: widget.builder,
+    );
+  }
 }
